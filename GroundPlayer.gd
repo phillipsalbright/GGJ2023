dd5f2ab5c0b58fcb3d0ba79242bb319c3ceb4458
Phillips Albright <55517109+phillipsalbright@users.noreply.github.com>
2023-02-03 17:29:03 -0500

    rolling feature

--- GroundPlayer.gd
+++ GroundPlayer.gd
@@ -10,6 +10,15 @@ var fall_acceleration = -210
 var speed = 70
 var snapDist = 5000
 var jump_force = 100
+var roll_time = 0
+var roll_damage_scale = .5
+var roll_launch_scale = 220
+var max_charge_time = 3
+var launching_time = 1
+var current_launch_timer = 0
+var last_direction = -1
+onready var sprite = get_node("CollisionShape2D/Sprite")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -19,10 +28,36 @@ func _ready():
 func _process(delta):
 	velocity.y -= fall_acceleration * delta;
 	var snap_vector = Vector2.ZERO
-	if is_on_floor() and Input.is_action_pressed("jump"):
-		velocity.y -= jump_force
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	if direction != 0:
+		last_direction = sign(direction)
+	
+	
+	if current_launch_timer > 0:
+		current_launch_timer -= delta
+		if current_launch_timer < 0:
+			sprite.modulate = Color(1, 1, 1, 1)
 	else:
-		snap_vector = Vector2.DOWN * snapDist;
-	velocity.x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * speed
+		velocity.x = (direction) * speed
+		if is_on_floor() and Input.is_action_pressed("jump"):
+			velocity.y -= jump_force
+		else:
+			snap_vector = Vector2.DOWN * snapDist;
+		if Input.is_action_pressed("special") and roll_time < max_charge_time:
+			velocity.x = 0;
+			roll_time += delta
+			sprite.modulate = Color(1, 0, 0, 1)
+		elif roll_time > 0:
+			velocity.x = roll_time * roll_launch_scale * last_direction
+			velocity.y = roll_time * roll_launch_scale * -.1
+			sprite.modulate = Color(0, 1, 0, 1)
+			current_launch_timer = launching_time
+			roll_time = 0
 	velocity = move_and_slide_with_snap(velocity, snap_vector, Vector2.UP, true, 4, .8)
 #	pass
+
+
+
+
+func _on_Area2D_area_entered(area):
+	print_debug("floppy")
