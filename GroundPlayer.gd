b418093e4fb9687597da06184c96fc9383cddec3
Phillips Albright <55517109+phillipsalbright@users.noreply.github.com>
2023-02-03 11:34:32 -0500

    added input maps, worked on player

--- GroundPlayer.gd
+++ GroundPlayer.gd
@@ -6,7 +6,10 @@ extends KinematicBody2D
 # var b = "text"
 
 var velocity = Vector2.ZERO
-var fall_acceleration = -210;
+var fall_acceleration = -210
+var speed = 70
+var snapDist = 5000
+var jump_force = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,8 +18,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity.y -= fall_acceleration * delta;
-	if is_on_floor() and Input.is_action_pressed("Jump"):
-		print_debug("frog")
-		velocity.y -= 100
-	velocity = move_and_slide(velocity, Vector2.UP)
+	var snap_vector = Vector2.ZERO
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		velocity.y -= jump_force
+	else:
+		snap_vector = Vector2.DOWN * snapDist;
+	velocity.x = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * speed
+	velocity = move_and_slide_with_snap(velocity, snap_vector, Vector2.UP, true, 4, .8)
 #	pass
